450197ca0afc45280d760e42310467c195dd81e6
Vinay <abhishaw5339@gmail.com>
2024-12-08 23:04:09 +0530

    Implemented Email send support.

--- src/test/java/com/vinay/project/uber/uberApp/UberAppApplicationTests.java
+++ src/test/java/com/vinay/project/uber/uberApp/UberAppApplicationTests.java
@@ -1,13 +1,36 @@
 package com.vinay.project.uber.uberApp;
 
+import com.vinay.project.uber.uberApp.services.EmailSenderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UberAppApplicationTests {
 
+	@Autowired
+	private EmailSenderService emailSenderService;
+
 	@Test
 	void contextLoads() {
+		emailSenderService.sendEmail(
+				"peyapoc163@datingel.com",
+				"This is the Testing Email.",
+				"Body of my email."
+		);
+	}
+
+	@Test
+	void sendEmailMultiple() {
+		String[] emails ={
+				"peyapoc163@datingel.com",
+				"homemypc007@gmail.com"
+		};
+		emailSenderService.sendEmail(
+				emails,
+				"Uber Mail testing",
+				"Hello World"
+		);
 	}
 
 }
